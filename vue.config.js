5673aa268dd81b352f9c3a18705896e28c2d2ef5
Jasid Martinez <jasidmartinez21@gmail.com>
2023-11-21 14:18:10 -0500

    Complete portfolio v1

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/portfolio_2/'
+  : '/',
   css: {
     loaderOptions: {
       scss: {
