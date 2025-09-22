d423970ed7b619829f5407f877d78670a5a6f212
Siternet <siternet.21@gmail.com>
2025-09-22 09:52:44 -0500

    Reordeamiento de los headers

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,71 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue';
+import { useHead } from '@vueuse/head';
+import { computed, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-const { locale } = useI18n();
+const { locale, t } = useI18n();
+useHead({
+  title: t('seo.title'),
+  meta: [
+    {
+      name: 'description',
+      content: computed(() => t('seo.meta.description')),
+    },
+    {
+      property: 'og:title',
+      content: computed(() => t('seo.meta.og.title')),
+    },
+    {
+      property: 'og:description',
+      content: computed(() => t('seo.meta.og.description')),
+    },
+    {
+      property: 'og:type',
+      content: 'website'
+    },
+    {
+      property: 'og:url',
+      content: 'https://hauntedbyte.site'
+    },
+    {
+      property: 'og:image',
+      content: 'https://www.hauntedbyte.site/hauntedbyte_preview.png'
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image'
+    },
+    {
+      name: 'twitter:title',
+      content: computed(() => t('seo.meta.twitter.title')),
+    },
+    {
+      name: 'twitter:description',
+      content: computed(() => t('seo.meta.twitter.description')),
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://www.hauntedbyte.site/hauntedbyte_preview.png'
+    }
+  ],
+  link: [
+    { rel: 'canonical', href: 'https://www.hauntedbyte.site' }
+  ],
+  script: [
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "url": "https://hauntedbyte.site",
+        "logo": "https://hauntedbyte.site/favicon.png"
+      })
+    }
+  ],
+});
+
 onMounted(() => {
   const language = navigator.language.split('-')[0];
   locale.value = ['es', 'en'].includes(language) ? language : 'en';
